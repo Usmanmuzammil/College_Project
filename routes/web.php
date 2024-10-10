c07bacff2ef4b563da966db4a519fb03f39c3621
Usmanmuzammil <muhammadmuzammilusman79@gmail.com>
2024-10-11 02:31:01 +0500

    set assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/admin', [LoginController::class, 'index'])->name('admin.login-view');
+Route::POST('/admin', [LoginController::class, 'adminLogin'])->name('admin.login');
+
+Route::prefix('/admin')->middleware('auth:admin')->group(function() {
+    Route::POST('/logout',[LoginController::class,'Logout'])->name('logout');
+
+    Route::get('/dashboard',[HomeController::class,'index'])->name('dashboard.page');
+    Route::get('/profile_update/view', [LoginController::class, 'profile_update_view']);
+    Route::post('/update_profile',[LoginController::class , 'update_profile'])->name('update_profile');
 });
