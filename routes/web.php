543785061753d268e054108a2d39c0a266f6ace5
Usmanmuzammil <muhammadmuzammilusman79@gmail.com>
2024-10-15 14:03:28 +0500

    layout

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('layout.webistelayout');
+});
 
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
